8cb6d404ed353636dd7566252ad5cf8e6f75c095
Naren Medarametla <naren@Narens-MacBook-Pro.local>
2025-01-25 20:44:22 +0530

    Created Repo

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(line);
+        scanner.close();
+    }
+}
